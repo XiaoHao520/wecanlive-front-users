581387d4bd59892ea1ec854baf30a035e1bbf641
pjb1994 <422061418@qq.com>
2017-06-26 15:02:39 +0800

    登錄頁面

--- src/components/passport/SignIn.vue
+++ src/components/passport/SignIn.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>此页面尚未实现</div>
+  <div class="page-signin">
+    <div class="wecan-icon"></div>
+
+    <div class="login-block">
+      <div class="login-way">
+        <a class="login-btn facebook" href="javascript:;"></a>
+        <router-link to="/signin/mobile" class="login-btn mobile" href="javascript:;"></router-link>
+        <a class="login-btn google" href="javascript:;"></a>
+      </div>
+
+      <div class="login-warn">
+        <div class="warn-text">登入即代表您同意</div>
+        <router-link to="/license"></router-link>
+        <div class="warn-text">的服務條款與隱私權政策</div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script type="text/babel" lang="babel">
@@ -14,4 +31,71 @@
 <style rel="stylesheet/less" type="text/less" lang="less" scoped>
   @import (once) '../../vue2-front/assets/css/less-template/template';
   @import (once) '../../assets/css/defines';
+
+  .page-signin {
+    background: svg-gradient(to bottom, #D500FE, #21AEDA);
+    padding-top: 280*@px;
+    .wecan-icon {
+      width: 400*@px;
+      height: 400*@px;
+      margin: 0 auto;
+      background: url("../../assets/image/A/startpage_logo@3x.png") 50% 50% no-repeat;
+      background-size: 100%;
+    }
+    .login-block {
+      position: absolute;
+      bottom: 75*@px;
+      left: 0;
+      right: 0;
+      text-align: center;
+      .login-way {
+        display: inline-block;
+        .clearfix();
+        .login-btn {
+          width: 100*@px;
+          height: 100*@px;
+          display: block;
+          background: 50% 50% no-repeat;
+          background-size: 100%;
+          float: left;
+          margin-right: 40*@px;
+          &:last-child {
+            margin-right: 0;
+          }
+          &.facebook {
+            background-image: url("../../assets/image/A/startpage_icon_facebook@3x.png");
+          }
+          &.mobile {
+            background-image: url("../../assets/image/A/startpage_icon_phone@3x.png");
+          }
+          &.google {
+            background-image: url("../../assets/image/A/startpage_icon_google@3x.png");
+          }
+        }
+      }
+      .login-warn {
+        margin-top: 20*@px;
+        font-size: 24*@px;
+        color: #fff;
+        display: inline-block;
+        .clearfix();
+        .warn-text {
+          float: left;
+          line-height: 30*@px;
+          height: 30*@px;
+        }
+        a {
+          float: left;
+          height: 24*@px;
+          display: inline-block;
+          width: 120*@px;
+          background: url("../../assets/image/A/font_wencan@3x.png") 50% 50% no-repeat;
+          background-size: 100%;
+          margin: 0 5*@px;
+        }
+      }
+    }
+
+  }
+
 </style>
--- src/components/passport/SignInMobile.vue
+++ src/components/passport/SignInMobile.vue
@@ -15,12 +15,17 @@
         <div class="row-label">
           <div class="icon pass-icon"></div>
         </div>
-        <input type="text" class="row-input" placeholder="請輸入手機號">
+        <input type="text" class="row-input" placeholder="請輸入密碼">
       </div>
 
 
       <div class="submit-row">
-        <a href="javascript:;">登錄</a>
+        <a class="btn" href="javascript:;">登錄</a>
+      </div>
+
+      <div class="action-row">
+        <a class="btn forgot-btn" href="javascript:;">忘記密碼</a>
+        <a class="btn register-btn" href="javascript:;">立即註冊</a>
       </div>
 
 
@@ -43,14 +48,81 @@
 
   .signin-mobile {
     background: #E5E5EC;
-    .blur(20px);
     .signin-mobile-block {
       padding: 30*@px 30*@px 0 30*@px;
       -webkit-box-sizing: border-box;
       -moz-box-sizing: border-box;
       box-sizing: border-box;
       .row {
-
+        height: 92*@px;
+        background: #fff;
+        padding-left: 192*@px;
+        box-sizing: border-box;
+        position: relative;
+        margin-bottom: 30*@px;
+        border-radius: 45*@px;
+        overflow: hidden;
+        &.pass-row {
+          margin-bottom: 60*@px;
+        }
+        .row-label {
+          font-size: 32*@px;
+          position: absolute;
+          left: 50*@px;
+          top: 24*@px;
+          .clearfix();
+          .icon {
+            float: left;
+            width: 44*@px;
+            height: 44*@px;
+            background: 50% 50% no-repeat;
+            background-size: 100%;
+            &.mobile-icon {
+              background-image: url("../../assets/image/A/login_icon_phone@3x.png") ;
+            }
+            &.pass-icon {
+              background-image: url("../../assets/image/A/login_icon_password@3x.png");
+            }
+          }
+          .mobile-location {
+            float: left;
+            margin-left: 8*@px;
+            line-height: 44*@px;
+          }
+        }
+        .row-input {
+          width: 100%;
+          padding: 0;
+          border: 0;
+          height: 100%;
+          font-size: 32*@px;
+          .placeholder-color(#959595);
+        }
+      }
+      .submit-row {
+        margin-bottom: 15*@px;
+        .btn {
+          border-radius: 45*@px;
+          color: #fff;
+          background: #3abbf0;
+          display: block;
+          height: 90*@px;
+          line-height: 90*@px;
+          text-decoration: none;
+          text-align: center;
+          font-size: 40*@px;
+        }
+      }
+      .action-row {
+        padding: 0 50*@px;
+        .btn {
+          font-size: 32*@px;
+          color: #959595;
+          text-decoration: none;
+          &.register-btn {
+            float: right;
+          }
+        }
       }
 
     }

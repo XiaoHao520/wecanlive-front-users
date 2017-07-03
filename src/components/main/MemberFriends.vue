9404ac06a766ed39c2d4aac89750f13f681cb9dc
pjb1994 <422061418@qq.com>
2017-07-03 16:23:01 +0800

    我的朋友

--- src/components/main/MemberFriends.vue
+++ src/components/main/MemberFriends.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>此页面尚未实现</div>
+  <div>
+    <header-common title="朋友(10)">
+      <div slot="right" class="scan-btn">
+        <a href="javascript:;" class="scan-icon"></a>
+      </div>
+    </header-common>
+
+
+    <div class="member-friends">
+      <div class="member-info">
+
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script type="text/babel" lang="babel">
@@ -14,4 +28,10 @@
 <style rel="stylesheet/less" type="text/less" lang="less" scoped>
   @import (once) '../../vue2-front/assets/css/less-template/template';
   @import (once) '../../assets/css/defines';
+  .member-friends {
+    .member-info {
+      height: 252*@px;
+      background: @bg-header;
+    }
+  }
 </style>

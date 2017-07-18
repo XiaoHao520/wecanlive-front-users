d78cbf4c2d6f00e1d8701d8f1ecdc4df8f14f26f
pjb1994 <422061418@qq.com>
2017-07-18 10:00:47 +0800

    鑽石兌換明細

--- src/components/main/PersonalDiamondTransactions.vue
+++ src/components/main/PersonalDiamondTransactions.vue
@@ -4,34 +4,18 @@
 
     <div class="transactions-list">
       <ul>
-        <li class="transactions-item">
+        <li class="transactions-item" v-for="item in items">
           <div class="transactions-header">
             <div class="transactions-type">兌換金幣</div>
-            <div class="transactions-date">2017-05-07</div>
+            <div class="transactions-date">{{ item.date_create | date('yy-mm-dd') }}</div>
           </div>
           <div class="transactions-num">
             <div class="transactions-diamond">
-              30
+              {{ item.diamond_count }}
             </div>
 
             <div class="transactions-coin">
-              60
-            </div>
-          </div>
-        </li>
-
-        <li class="transactions-item">
-          <div class="transactions-header">
-            <div class="transactions-type">兌換金幣</div>
-            <div class="transactions-date">2017-05-07</div>
-          </div>
-          <div class="transactions-num">
-            <div class="transactions-diamond">
-              30
-            </div>
-
-            <div class="transactions-cash">
-              60
+              {{ item.coins_count }}
             </div>
           </div>
         </li>
@@ -42,8 +26,21 @@
 
 <script type="text/babel" lang="babel">
   export default {
+    data() {
+      return {
+        items: [],
+      };
+    },
     methods: {
       reload() {
+        const vm = this;
+        // todo: 兌換現金流水
+        vm.api('DiamondExchangeRecord').get({
+          author: vm.me.id,
+        }).then((resp) => {
+          vm.items = resp.data.results;
+          console.log(vm.items);
+        });
       },
     },
   };
@@ -58,7 +55,9 @@
       padding: 30*@px 30*@px 0 30*@px;
       position: absolute;
       top: @height-header;
-      bottom: 0; left: 0; right: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
       overflow-y: scroll;
       .app-scroll();
       .transactions-item {
